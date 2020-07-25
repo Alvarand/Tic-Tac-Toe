43bc42a4138483875d307d519b53d66c4c043d1c
Alvarand <dgalil.1999@mail.ru>
2020-07-26 00:11:16 +0300

    fixed any problems
    You can win on your last turn

--- main.py
+++ main.py
@@ -20,49 +20,48 @@ def b1(event):
 
 def check():
     global MAP
-    if not (0 in MAP[0] or 0 in MAP[1] or 0 in MAP[2]):
+    if MAP[0][0] == MAP[1][1] == MAP[2][2] and MAP[0][0] != 0:
+        if MAP[1][1] == 'x':
+            tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
+        else:
+            tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
+    elif MAP[0][2] == MAP[1][1] == MAP[2][0] and MAP[1][1] != 0:
+        if MAP[1][1] == 'x':
+            tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
+        else:
+            tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
+    elif MAP[0][0] == MAP[0][1] == MAP[0][2] and MAP[0][0] != 0:
+        if MAP[0][0] == 'x':
+            tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
+        else:
+            tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
+    elif MAP[1][0] == MAP[1][1] == MAP[1][2] and MAP[1][0] != 0:
+        if MAP[1][0] == 'x':
+            tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
+        else:
+            tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
+    elif MAP[2][0] == MAP[2][1] == MAP[2][2] and MAP[2][0] != 0:
+        if MAP[1][0] == 'x':
+            tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
+        else:
+            tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
+    elif MAP[0][0] == MAP[1][0] == MAP[2][0] and MAP[0][0] != 0:
+        if MAP[0][0] == 'x':
+            tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
+        else:
+            tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
+    elif MAP[0][1] == MAP[1][1] == MAP[2][1] and MAP[0][1] != 0:
+        if MAP[0][1] == 'x':
+            tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
+        else:
+            tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
+    elif MAP[0][2] == MAP[1][2] == MAP[2][2] and MAP[0][2] != 0:
+        if MAP[0][2] == 'x':
+            tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
+        else:
+            tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
+    elif not (0 in MAP[0] or 0 in MAP[1] or 0 in MAP[2]):
         tkinter.messagebox.showinfo("Ничья", 'Ничья')
-    else:
-        if MAP[0][0] == MAP[1][1] == MAP[2][2] and MAP[0][0] != 0:
-            if MAP[1][1] == 'x':
-                tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
-            else:
-                tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
-        elif MAP[0][2] == MAP[1][1] == MAP[2][0] and MAP[1][1] != 0:
-            if MAP[1][1] == 'x':
-                tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
-            else:
-                tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
-        elif MAP[0][0] == MAP[0][1] == MAP[0][2] and MAP[0][0] != 0:
-            if MAP[0][0] == 'x':
-                tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
-            else:
-                tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
-        elif MAP[1][0] == MAP[1][1] == MAP[1][2] and MAP[1][0] != 0:
-            if MAP[1][0] == 'x':
-                tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
-            else:
-                tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
-        elif MAP[2][0] == MAP[2][1] == MAP[2][2] and MAP[2][0] != 0:
-            if MAP[1][0] == 'x':
-                tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
-            else:
-                tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
-        elif MAP[0][0] == MAP[1][0] == MAP[2][0] and MAP[0][0] != 0:
-            if MAP[0][0] == 'x':
-                tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
-            else:
-                tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
-        elif MAP[0][1] == MAP[1][1] == MAP[2][1] and MAP[0][1] != 0:
-            if MAP[0][1] == 'x':
-                tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
-            else:
-                tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
-        elif MAP[0][2] == MAP[1][2] == MAP[2][2] and MAP[0][2] != 0:
-            if MAP[0][2] == 'x':
-                tkinter.messagebox.showinfo("Победа", 'Первый игрок победил')
-            else:
-                tkinter.messagebox.showinfo("Победа", 'Второй игрок победил')
 
 
 def escape(event):
